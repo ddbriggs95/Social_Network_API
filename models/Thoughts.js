6258058a40b1077b6364dc7ac6cd1bcd3f015efb
ddbriggs95 <dowlingbriggs@gmail.com>
2023-05-31 16:18:24 -0500

    Created Thoughts model and Reactions scehma as a subdocument schema

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,6 +1,40 @@
 const {Schema, model, Types} = require('mongoose');
 const moment = require('moment');
 
+
+//ReactionsSchema
+
+const ReactionsSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+)
+
+//thoughts schema
 const ThoughtsSchema = new Schema(
     {
         thoughtText: {
@@ -28,3 +62,16 @@ const ThoughtsSchema = new Schema(
         id: false
     }
 );
+
+
+// get total friends
+
+ThoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+//create Thoughts model
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+// export Thoughts model
+module.exports = Thoughts;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
+//rewquire Users and Thoughts moddels
 const Users = require('./Users');
 const Thoughts = require('./Thoughts');
 
+//export Users and Thoughts models
 module.exports = { Users, Thoughts};
